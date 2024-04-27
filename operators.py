e3fe464522ac42b2799854060f94331de8ab5e95
shravanirajulu2004 <167171556+shravanirajulu2004@users.noreply.github.com>
2024-04-27 09:22:11 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,19 @@
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    sum=a+b
+    print(sum)
+    
+    diff=a-b
+    print(diff)
+    
+    product=a*b
+    print(product)
+    
